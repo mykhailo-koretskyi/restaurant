7e07c801b16aa9d5f37557493c7f584128b25ef1
Mykhailo Koretskyi <mykhailo.koretskyi@gmail.com>
2014-12-17 16:27:42 +0200

    Including controller specific .js and .css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w(
+    landing.js landing.css
+    configuration/sections.js configuration/sections.css
+    configuration/landing.js configuration/landing.css
+)
